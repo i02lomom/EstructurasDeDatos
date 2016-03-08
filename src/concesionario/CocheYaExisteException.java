46fd53041034ceaca8470c8ee777f3cdaf49fe8b
Miguel Ángel López Moyano <i02lomom@gmail.com>
2016-03-08 23:32:31 +0100

    Create CocheYaExisteException.java

---
+++ src/concesionario/CocheYaExisteException.java
@@ -0,0 +1,9 @@
+package concesionario;
+
+public class CocheYaExisteException extends Exception {
+	private static final long serialVersionUID = 1L;
+
+	public CocheYaExisteException(String string){
+		super(string);
+	}
+}
