a32594e89eb62e6cdc7a46a75cf6a27709b704f4
Miguel Ángel López Moyano <i02lomom@gmail.com>
2016-03-08 23:41:37 +0100

    Create ModeloNoValidoException.java

---
+++ src/concesionario/ModeloNoValidoException.java
@@ -0,0 +1,9 @@
+package concesionario;
+
+public class ModeloNoValidoException extends Exception {
+	private static final long serialVersionUID = 1L;
+	
+	public ModeloNoValidoException(String string){
+		super(string);
+	}
+}
