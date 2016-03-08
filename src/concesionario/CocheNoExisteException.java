e0250404275853b8563333f426ef5df765ef2c3f
Miguel Ángel López Moyano <i02lomom@gmail.com>
2016-03-08 23:31:48 +0100

    Create CocheNoExisteException.java

---
+++ src/concesionario/CocheNoExisteException.java
@@ -0,0 +1,9 @@
+package concesionario;
+
+public class CocheNoExisteException extends Exception {
+	private static final long serialVersionUID = 1L;
+
+	public CocheNoExisteException(String string){
+		super(string);
+	}
+}
