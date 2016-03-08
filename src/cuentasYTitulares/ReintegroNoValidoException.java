c63dfc52665cda60140e74b03323266b345b8eb1
Miguel Ángel López Moyano <i02lomom@gmail.com>
2016-03-08 23:04:33 +0100

    Create ReintegroNoValidoException.java

---
+++ src/cuentasYTitulares/ReintegroNoValidoException.java
@@ -0,0 +1,9 @@
+package cuentasYTitulares;
+
+public class ReintegroNoValidoException extends Exception {
+	private static final long serialVersionUID = 1L;
+
+	public ReintegroNoValidoException(String string){
+		super(string);
+	}
+}
