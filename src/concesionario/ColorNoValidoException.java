bb7ab796b33e4b08b3f97adff1b9e7c049efbc34
Miguel Ángel López Moyano <i02lomom@gmail.com>
2016-03-08 23:33:54 +0100

    Create ColorNoValidoException.java

---
+++ src/concesionario/ColorNoValidoException.java
@@ -0,0 +1,10 @@
+package concesionario;
+
+public class ColorNoValidoException extends Exception {
+	private static final long serialVersionUID = 1L;
+	
+	public ColorNoValidoException(String string){
+		super(string);
+	}
+
+}
