8e4765f2951dde1e7cf555ae12b4cead0cf8aff8
Miguel Ángel López Moyano <i02lomom@gmail.com>
2016-03-08 23:40:21 +0100

    Create MatriculaNoValidaException.java

---
+++ src/concesionario/MatriculaNoValidaException.java
@@ -0,0 +1,9 @@
+package concesionario;
+
+public class MatriculaNoValidaException extends Exception {
+	private static final long serialVersionUID = 1L;
+	
+	public MatriculaNoValidaException(String string){
+		super(string);
+	}
+}
