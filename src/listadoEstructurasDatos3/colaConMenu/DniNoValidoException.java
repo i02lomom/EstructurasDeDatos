7a622b0aaa2ea5656fd5723d2e5743effe79e7f1
Miguel Ángel López Moyano <i02lomom@gmail.com>
2016-03-09 09:49:43 +0100

    Create DniNoValidoException.java

---
+++ src/listadoEstructurasDatos3/colaConMenu/DniNoValidoException.java
@@ -0,0 +1,9 @@
+package listadoEstructurasDatos3.colaConMenu;
+
+public class DniNoValidoException extends Exception {
+	private static final long serialVersionUID = 1L;
+
+	public DniNoValidoException(String string) {
+		super(string);
+	}
+}
