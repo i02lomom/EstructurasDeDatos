533d6af58d02b9024b32badbc85e62a16d08b216
Miguel Ángel López Moyano <i02lomom@gmail.com>
2016-03-09 09:51:51 +0100

    Create EdadNoValidaException.java

---
+++ src/listadoEstructurasDatos3/colaConMenu/EdadNoValidaException.java
@@ -0,0 +1,9 @@
+package listadoEstructurasDatos3.colaConMenu;
+
+public class EdadNoValidaException extends Exception {
+	private static final long serialVersionUID = 1L;
+
+	public EdadNoValidaException(String string) {
+		super(string);
+	}
+}
