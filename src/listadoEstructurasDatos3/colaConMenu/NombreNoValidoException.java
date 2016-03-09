58eacc84d7a9fb147055fd75edd39f8a1fa2446a
Miguel Ángel López Moyano <i02lomom@gmail.com>
2016-03-09 09:54:07 +0100

    Create NombreNoValidoException.java

---
+++ src/listadoEstructurasDatos3/colaConMenu/NombreNoValidoException.java
@@ -0,0 +1,9 @@
+package listadoEstructurasDatos3.colaConMenu;
+
+public class NombreNoValidoException extends Exception {
+	private static final long serialVersionUID = 1L;
+
+	public NombreNoValidoException(String string) {
+		super(string);
+	}
+}
