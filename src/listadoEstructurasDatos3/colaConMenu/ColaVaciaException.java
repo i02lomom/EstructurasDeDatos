c9d12bfa4983430fa7a74fb744717a1e814b7c50
Miguel Ángel López Moyano <i02lomom@gmail.com>
2016-03-09 09:48:21 +0100

    Create ColaVaciaException.java

---
+++ src/listadoEstructurasDatos3/colaConMenu/ColaVaciaException.java
@@ -0,0 +1,9 @@
+package listadoEstructurasDatos3.colaConMenu;
+
+public class ColaVaciaException extends Exception {
+	private static final long serialVersionUID = 1L;
+
+	public ColaVaciaException(String string) {
+		super(string);
+	}
+}
