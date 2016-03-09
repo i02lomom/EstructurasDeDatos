b5c81266e9464ea12bae150679b9ff03da628d70
Miguel Ángel López Moyano <i02lomom@gmail.com>
2016-03-09 09:40:32 +0100

    Create ApellidosNoValidosException.java

---
+++ src/listadoEstructurasDatos3/colaConMenu/ApellidosNoValidosException.java
@@ -0,0 +1,9 @@
+package listadoEstructurasDatos3.colaConMenu;
+
+public class ApellidosNoValidosException extends Exception {
+	private static final long serialVersionUID = 1L;
+
+	public ApellidosNoValidosException(String string) {
+		super(string);
+	}
+}
