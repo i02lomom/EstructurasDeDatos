f0684291fab8c5c352029b30e5de91b4598331d1
Miguel Ángel López Moyano <i02lomom@gmail.com>
2016-03-10 09:46:02 +0100

    Create Autoboxing.java

---
+++ src/listadoEstructurasDatos/autoboxing/Autoboxing.java
@@ -0,0 +1,19 @@
+package listadoEstructurasDatos.autoboxing;
+
+/**
+ * ¿Qué es el autoboxing/unboxing? Explícalo mediante tu propio código en Java 
+ * “autoboxing.java”
+ * 
+ * @author Miguel Angel López Moyano
+ * @version 1.0
+ */
+public class Autoboxing {
+
+	public static void main(String[] args) {
+		Integer a=2;
+		int b=3;
+
+		int c=a+b;
+		System.out.println(a+" + "+b+" = "+c);
+	}
+}
